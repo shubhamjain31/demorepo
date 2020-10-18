8ba22be3b56575c3efce311b0c7473cd1ae6c8f9
shubhamjain31 <sj27754@gmail.com>
2020-10-18 18:28:39 +0530

    Heroku Demo Project

--- herokuproject/herokuproject/settings.py
+++ herokuproject/herokuproject/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o*qwxqks(5y^=q31^y0p$hk$fgb@ex4h8t%ss7lh0g5zoai8jd'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
