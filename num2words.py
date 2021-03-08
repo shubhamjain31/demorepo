d2ba0b4134eae36dd4dfd6da0dbe8ca6626f7e13
shubhamjain31 <sj27754@gmail.com>
2021-03-08 21:26:02 +0530

    number to words code

---
+++ num2words.py
@@ -0,0 +1,27 @@
+import decimal    
+
+def num2words(num):
+    num = decimal.Decimal(num)
+    decimal_part = num - int(num)
+    num = int(num)
+
+    if decimal_part:
+        return num2words(num) + " point " + (" ".join(num2words(i) for i in str(decimal_part)[2:]))
+
+    under_20 = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+    tens = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+    above_100 = {100: 'Hundred', 1000: 'Thousand', 100000: 'Lakhs', 10000000: 'Crores'}
+
+    if num < 20:
+        return under_20[num]
+
+    if num < 100:
+        return tens[num // 10 - 2] + ('' if num % 10 == 0 else ' ' + under_20[num % 10])
+
+    # find the appropriate pivot - 'Million' in 3,603,550, or 'Thousand' in 603,550
+    pivot = max([key for key in above_100.keys() if key <= num])
+
+    return num2words(num // pivot) + ' ' + above_100[pivot] + ('' if num % pivot==0 else ' ' + num2words(num % pivot))
+
+
+print(num2words(decimal.Decimal("238484")))
