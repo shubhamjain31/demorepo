235faed27e43ec1e1b7191ae284320ccd43fb3c9
shubhamjain31 <sj27754@gmail.com>
2021-06-11 21:48:22 +0530

    Change create_backup function

--- Django_DB_Backup/App/views.py
+++ Django_DB_Backup/App/views.py
@@ -17,31 +17,38 @@ def index(request):
 	return render(request, 'index.html')
 
 def create_backup(request):
+	_pk = 11
 
 	# NestedObjects is admin contrib package which is used as a Collector subclass.
 	collector = NestedObjects(using="default") # database name
 
 	# create an object of NestedObjects
-	collector.collect([User.objects.get(pk=7)])
+	collector.collect([User.objects.get(pk=_pk)])
 
 	# create a list of all objects of all tables with foreign keys
 	objects = list(chain.from_iterable(collector.data.values()))
 
 	# store a data in file
-	with open("backup_export1.json", "w") as f:
+	with open("backup_export3.json", "w") as f:
 		s = serializers.serialize("json", objects, use_natural_foreign_keys=True, use_natural_primary_keys=True, indent = 4)
-		s = re.sub('"pk": [0-9]{1,5}', '"pk": null', s)
+		
+		# make all tables objects pks null
+		# s = re.sub('"pk": [0-9]{1,5}', '"pk": null', s)
 		f.write(s)
 
-	# delete user
-	User.objects.get(pk=7).delete()
+	# user object
+	user = User.objects.get(pk=_pk)
+
+	# delete all relation of user object
+	Post.objects.filter(author=user).delete()
+	Description.objects.filter(post_desc=user).delete()
 	return HttpResponse('DOne')
 
 
 def restore_backup(request):
 
 	# file name
-	filename = 'backup_export1'
+	filename = 'backup_export3'
 
 	# use call command for restore a data
 	call_command('loaddata', '{}'.format(filename))
