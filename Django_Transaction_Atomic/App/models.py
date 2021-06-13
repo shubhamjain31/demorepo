e7befcf725cd2617f27777a002d07c43a859b391
shubhamjain31 <sj27754@gmail.com>
2021-06-13 13:37:45 +0530

    Added transaction atomic functionality

--- Django_Transaction_Atomic/App/models.py
+++ Django_Transaction_Atomic/App/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Payment(models.Model):
+	user  	= models.CharField(max_length=100)
+	amount  = models.IntegerField(default=0)
+
+	def __str_(self) -> str:
+		return self.user
--- Django_Transaction_Atomic/App/views.py
+++ Django_Transaction_Atomic/App/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from django.db import transaction
+from .models import *
 
 # Create your views here.
+
+
+def index(request):
+	if request.method == 'POST':
+		try:
+			user_one = request.POST.get('user_one')
+			user_two = request.POST.get('user_two')
+			amount   = request.POST.get('amount')
+
+			# used for commit rollback
+			with transaction.atomic():
+				user_one_obj = Payment.objects.get(user = user_one.title())
+				user_one_obj.amount -= int(amount)
+				user_one_obj.save()
+
+				user_two_obj = Payment.objects.get(user = user_two.title())
+				user_two_obj.amount += int(amount)
+				user_two_obj.save()
+
+				messages.success(request, 'Your amount is transfered')
+		except Exception as e:
+			print(e)
+			messages.error(request, 'Something went wrong')
+
+		return redirect('/')
+	return render(request, 'index.html')
