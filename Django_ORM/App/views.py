9dd072ad1edd6047b46f779321598b7d8f66f32c
shubhamjain31 <sj27754@gmail.com>
2021-08-08 12:07:45 +0530

    add queries views

--- Django_ORM/App/views.py
+++ Django_ORM/App/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+
 from .models import *
 
 # Create your views here.
@@ -10,15 +12,26 @@ def index(request):
 	return HttpResponse('Done')
 
 def add_student(request):
-	student = Student.objects.create(first_name="Shubham", last_name="Jain", age=26, email="shubhamjain@gmail.com", 
-				mobile="9876543312", website="http://www.mywebsite.com/", section="Alpha")
-	return HttpResponse('Done')
+	student = Student.objects.create(first_name="Sarthak", 
+									last_name="Jain", 
+									age=24, 
+									email="sarthak31@gmail.com", 
+									mobile="8863638291", 
+									website="http://www.sarthakcool.com/",
+									section="B")
+	return HttpResponse('<h1>Student Added</h1>')
 
 def add_teacher(request):
-	return HttpResponse('Done')
+	user = User.objects.last()
+
+	teacher = Teacher.objects.create(name="Suniel Singh",
+									user=user)
+	return HttpResponse('<h1>Teacher Added</h1>')
 
 def add_classroom(request):
-	return HttpResponse('Done')
+	# classroom = Classroom.objects.create(student="Suniel Singh",
+	# 									teacher=)
+	return HttpResponse('<h1>Classroom Added</h1>')
 
 def add_courses(request):
 	return HttpResponse('Done')
