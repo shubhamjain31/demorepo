d5143816ee2c620ae9698d1968013d7f261ed97a
shubhamjain31 <sj27754@gmail.com>
2022-06-28 10:49:43 +0530

    algorithm of hash table in DS

---
+++ Data_Structure/hash_table.py
@@ -0,0 +1,60 @@
+stock_prices = []
+with open('data.csv', 'r') as f:
+    for line in f:
+        day, price = line.split(',')
+        price = float(price)
+        stock_prices.append([day, price])
+
+# print(stock_prices)
+
+for element in stock_prices:
+    if element[0] == '6-Mar':
+        print(element[1])
+
+stock_prices = {}
+with open('data.csv', 'r') as f:
+    for line in f:
+        day, price = line.split(',')
+        price = float(price)
+        stock_prices[day] = price
+
+print(stock_prices)
+
+
+# ***************************************************************************************************************
+def get_hash(key):
+    sum = 0
+    for c in key:
+        sum += ord(c)
+    return sum % 100
+
+print(get_hash('6-Mar'))
+
+# ***************************************************************************************************************
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None]*self.MAX
+
+    def get_hash(self, key):
+        sum = 0
+        for c in key:
+            sum += ord(c)
+        return sum % self.MAX
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+h = HashTable()
+h['6-Mar'] = 5          # set item
+del h['6-Mar']          # del item
+print(h['6-Mar'])       # get item
---
+++ Data_Structure/hash_table_chaining.py
@@ -0,0 +1,44 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        hash = 0
+        for c in key:
+            hash += ord(c)
+        return hash % self.MAX
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for kv in self.arr[h]:
+            if kv[0] == key:
+                return kv[1]
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        found = False
+
+        for idx, element in enumerate(self.arr[h]):
+            print(idx,element, self.arr[h][idx])
+
+            if len(element) == 2 and element[0] == key:
+                self.arr[h][idx] =(key, val)
+                found = True
+        
+        if not found:
+            self.arr[h].append((key, val))
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+h = HashTable()
+h['6-Mar'] = 5              # set item
+h['6-Mar'] = 52          # set item
+h['8-Mar'] = 3          # set item
+h['9-Mar'] = 45          # set item
+h['17-Mar'] = 2          # set item
+h['20-Mar'] = 59          # set item
+# del h['6-Mar']          # del item
+print(h.arr)       # get item
